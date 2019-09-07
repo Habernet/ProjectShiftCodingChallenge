926299ff2402d65164198ffe5dc0ba2f7bd1f22e
habernet <phaberern@gmail.com>
2019-09-07 14:45:55 -0400

    Problem 2 complete

--- problems.js
+++ problems.js
@@ -2,3 +2,12 @@
 let alertDurhamStatus = () => {
   alert("Durham is awesome!");
 };
+
+// Problem 2
+const bands = ["Kiss", "Aerosmith", "ACDC", "Led Zeppelin", "Nickelback"];
+
+let bandAlertLooper = () => {
+  bands.forEach(band => {
+    alert(`I love ${band}!`);
+  });
+};
